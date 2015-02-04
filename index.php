56e68cb5c5d9ad6060e61f0d83120082a6767930
Pieter De Norre <pieter.de.norre@telenet.be>
2015-02-04 13:28:45 +0100

    v1.0
    FUN

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title></title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+		<link rel="stylesheet" href="stylesheets/styles.css">
+	</head>
+	<body>
+		<nav>
+			<div class="container">
+				<div class="main-area intro">
+					<ul>
+						<li>One</li>
+						<li>Two</li>
+						<li>Three</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+		<section id="one">
+			<div class="container">
+				<div class="main-area intro"></div>
+			</div>
+		</section>
+		<section id="two">
+		<div class="container">
+			<div class="main-area">
+				<div class="column content">
+					<p>This is the Content.</p>
+				</div>
+				<div class="column sidebar">
+					<p>This is the Sidebar.</p>
+				</div>
+				<div class="column side-nav">
+					<p>This is the Navigation.</p>
+				</div>
+			</div>
+		</div>
+		</section>
+	</body>
+</html>
